3053335463fa4fd01aff53a44ab3faaade88e07c
canonnk <729730350@qq.com>
2019-12-18 17:27:19 +0800

    添加菜单页面

---
+++ src/view/sys/menu-manager/menu-manager.vue
@@ -0,0 +1,50 @@
+<template>
+    <Row>
+        <Col span="12">
+            <Tree :data="data1"></Tree>
+        </Col>
+        <Col span="12">col-12</Col>
+    </Row>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            data1: [
+                {
+                    title: 'parent 1',
+                    expand: true,
+                    children: [
+                        {
+                            title: 'parent 1-1',
+                            expand: true,
+                            children: [
+                                {
+                                    title: 'leaf 1-1-1'
+                                },
+                                {
+                                    title: 'leaf 1-1-2'
+                                }
+                            ]
+                        },
+                        {
+                            title: 'parent 1-2',
+                            expand: true,
+                            children: [
+                                {
+                                    title: 'leaf 1-2-1'
+                                },
+                                {
+                                    title: 'leaf 1-2-1'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
+
+</script>
