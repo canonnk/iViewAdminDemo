7629707b2a6952eb503b30c6c50bbd64531d0f05
canonnk <729730350@qq.com>
2020-03-11 23:16:59 +0800

    提交

--- src/view/test001/Home.vue
+++ src/view/test001/Home.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        Home
+        {{displayText}}
     </div>
 </template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            displayText: 'here is home.vue'
+        }
+    }
+}
+</script>
--- src/view/test001/index.vue
+++ src/view/test001/index.vue
@@ -7,33 +7,25 @@
                 </Card>
             </div>
         </Content>
-        <Sider style="xx">
+        <Sider v-bind:collapsible="true" v-model="isHideReportList" hide-trigger v-bind:width="200" v-bind:collapsed-width="80">
             <div style="width:100%;height:100%;background-color:#F5F7F9;">
-                <Card style="width:100%;height:100%;">
-                    <List>
-                        <ListItem><p v-on:click="Switch(1)">报表1</p></ListItem>
-                        <ListItem><p v-on:click="Switch(2)">报表2</p></ListItem>
-                        <ListItem><p v-on:click="Switch(3)">报表3</p></ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
-                        <ListItem>报表1</ListItem>
+                <Card style="width:100%;height:100%;overflow-y: auto;" >
+                    <List style="padding:0;">
+                        <ListItem style="padding:0;">
+                            <Button type="primary" style="width:100%; padding-left:0;padding-right:0;" v-on:click="isHideReportList = !isHideReportList">
+                                <Icon type="md-arrow-round-back" v-if="isHideReportList" style="font-size:22px;"/>
+                                <Icon type="md-arrow-round-forward" v-else style="font-size:22px;"/>
+                            </Button>
+                        </ListItem>
+                        <ListItem style="padding:0;" v-for="item in Reports" v-bind:key="item.id">
+                            <Button
+                                v-bind:type="currentComponentName==item.id?'primary':'text'"
+                                v-on:click="Switch(item.id)"
+                                v-bind:style="'width:100%; color:black;padding-left:0;padding-right:0;' + (isHideReportList?'':'text-align:left;')"
+                                ghost>
+                                <Icon v-bind:type="item.icon" style="font-size:22px;" /><span style="padding-left:10px;" v-if="!isHideReportList">{{item.name}}</span>
+                            </Button>
+                        </ListItem>
                     </List>
                 </Card>
             </div>
@@ -49,21 +41,24 @@ import Foo from './Foo.vue'
 export default {
     data: function () {
         return {
-            currentTabComponent: Home
+            currentComponentName: '',
+            currentTabComponent: '',
+            isHideReportList: false,
+            Reports: [
+                { id: 'Home', component: Home, name: '报表1', icon: 'ios-appstore' },
+                { id: 'Foo', component: Foo, name: '报表2', icon: 'md-archive' },
+                { id: 'Bar', component: Bar, name: '报表3', icon: 'md-barcode' }
+            ]
         }
     },
     methods: {
-        Switch: function (index) {
-            switch (index) {
-            case 1:
-                this.currentTabComponent = Home
-                break
-            case 2:
-                this.currentTabComponent = Foo
-                break
-            case 3:
-                this.currentTabComponent = Bar
-                break
+        Switch: function (componentName) {
+            this.currentComponentName = componentName
+            for (let i = 0; i < this.Reports.length; i++) {
+                if (this.Reports[i].id === componentName) {
+                    this.currentTabComponent = this.Reports[i].component
+                    break
+                }
             }
         }
     }
@@ -71,5 +66,7 @@ export default {
 </script>
 
 <style>
-
+.x{
+ overflow-y: scroll;
+}
 </style>
